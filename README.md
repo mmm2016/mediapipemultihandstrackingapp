5ba1e016a924de3f4690ebcd8fdf7c90f21c2993
阿毛 <maogl@ktc.cn>
2020-06-20 09:05:51 +0800

    Hand movement recognition

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 Gesture recognition through Mediapipe on Android
 
-see these [instructions](https://google.github.io/mediapipe/getting_started/android_archive_library.html) that use Android Archive (AAR) and Gradle，The source code is copied from MediaPipe's [multi-hand tracking gpu demo](https://github.com/jiuqiant/mediapipe_multi_hands_tracking_aar_example). The idea of gesture recognition comes from [Simple Hand Gesture Recognition Code - Hand tracking - Mediapipe](https://gist.github.com/TheJLifeX/74958cc59db477a91837244ff598ef4a)
+see these [instructions](https://google.github.io/mediapipe/getting_started/android_archive_library.html) that use Android Archive (AAR) and Gradle，The source code is copied from MediaPipe's [multi-hand tracking gpu demo](https://github.com/jiuqiant/mediapipe_multi_hands_tracking_aar_example). The idea of gesture recognition comes from [Simple Hand Gesture Recognition Code - Hand tracking - Mediapipe](https://gist.github.com/TheJLifeX/74958cc59db477a91837244ff598ef4a)， [Hand movement recognition](https://github.com/TheJLifeX/mediapipe/tree/hand-mouvement-recognition)
+
+
+
+If it helps you, please give me a star 。
+
+
 
--- app/src/main/java/com/example/mediapipemultihandstracking/MainActivity.java
+++ app/src/main/java/com/example/mediapipemultihandstracking/MainActivity.java
@@ -20,7 +20,10 @@ import com.google.mediapipe.components.PermissionHelper;
 import com.google.mediapipe.formats.proto.LandmarkProto;
 import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmark;
 import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmarkList;
+import com.google.mediapipe.formats.proto.RectProto;
 import com.google.mediapipe.framework.AndroidAssetUtil;
+import com.google.mediapipe.framework.Packet;
+import com.google.mediapipe.framework.PacketCallback;
 import com.google.mediapipe.framework.PacketGetter;
 import com.google.mediapipe.glutil.EglManager;
 
@@ -35,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String INPUT_VIDEO_STREAM_NAME = "input_video";
     private static final String OUTPUT_VIDEO_STREAM_NAME = "output_video";
     private static final String OUTPUT_LANDMARKS_STREAM_NAME = "multi_hand_landmarks";
+    private static final String OUTPUT_HAND_RECT = "multi_hand_rects";
     private static final CameraHelper.CameraFacing CAMERA_FACING = CameraHelper.CameraFacing.FRONT;
     // Flips the camera-preview frames vertically before sending them into FrameProcessor to be
     // processed in a MediaPipe graph, and flips the processed frames back when they are displayed.
@@ -43,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
     private static final boolean FLIP_FRAMES_VERTICALLY = true;
 
     private TextView gesture;
+    private TextView moveGesture;
 
     static {
         // Load all native libraries needed by the app.
@@ -64,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
     private ExternalTextureConverter converter;
     // Handles camera access via the {@link CameraX} Jetpack support library.
     private CameraXPreviewHelper cameraHelper;
+    List<NormalizedLandmarkList> multiHandLandmarks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
         previewDisplayView = new SurfaceView(this);
         setupPreviewDisplayView();
         gesture = findViewById(R.id.gesture);
+        moveGesture = findViewById(R.id.move_gesture);
         // Initialize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
         // binary graphs.
         AndroidAssetUtil.initializeNativeAssetManager(this);
@@ -88,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
                 OUTPUT_LANDMARKS_STREAM_NAME,
                 (packet) -> {
                     Log.d(TAG, "Received multi-hand landmarks packet.");
-                    List<NormalizedLandmarkList> multiHandLandmarks =
+                    multiHandLandmarks =
                             PacketGetter.getProtoVector(packet, NormalizedLandmarkList.parser());
 
                     runOnUiThread(new Runnable() {
@@ -103,6 +110,27 @@ public class MainActivity extends AppCompatActivity {
                                     + packet.getTimestamp()
                                     + "] "
                                     + getMultiHandLandmarksDebugString(multiHandLandmarks));
+                });
+
+        processor.addPacketCallback(
+                OUTPUT_HAND_RECT
+                , new PacketCallback() {
+                    @Override
+                    public void process(Packet packet) {
+
+                        List<RectProto.NormalizedRect> normalizedRectsList = PacketGetter.getProtoVector(packet, RectProto.NormalizedRect.parser());
+
+                        try {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    moveGesture.setText(handGestureMoveCalculator(normalizedRectsList));
+                                }
+                            });
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
                 });
         PermissionHelper.checkAndRequestCameraPermissions(this);
     }
@@ -281,13 +309,122 @@ public class MainActivity extends AppCompatActivity {
         return "___";
     }
 
+    float previousXCenter;
+    float previousYCenter;
+    float previousAngle; // angle between the hand and the x-axis. in radian
+    float previous_rectangle_width;
+    float previousRectangleHeight;
+    boolean frameCounter;
+
+    private String handGestureMoveCalculator(List<RectProto.NormalizedRect> normalizedRectList) {
+
+        RectProto.NormalizedRect normalizedRect = normalizedRectList.get(0);
+        float height = normalizedRect.getHeight();
+        float centerX = normalizedRect.getXCenter();
+        float centerY = normalizedRect.getYCenter();
+        if (previousXCenter != 0) {
+            double mouvementDistance = getEuclideanDistanceAB(centerX, centerY,
+                    previousXCenter, previousYCenter);
+            // LOG(INFO) << "Distance: " << mouvementDistance;
+
+            double mouvementDistanceFactor = 0.02; // only large mouvements will be recognized.
+
+            // the height is normed [0.0, 1.0] to the camera window height.
+            // so the mouvement (when the hand is near the camera) should be equivalent to the mouvement when the hand is far.
+            double mouvementDistanceThreshold = mouvementDistanceFactor * height;
+            if (mouvementDistance > mouvementDistanceThreshold) {
+                double angle = radianToDegree(getAngleABC(centerX, centerY,
+                        previousXCenter, previousYCenter, previousXCenter + 0.1,
+                        previousYCenter));
+                // LOG(INFO) << "Angle: " << angle;
+                if (angle >= -45 && angle < 45) {
+                    return "Scrolling right";
+                } else if (angle >= 45 && angle < 135) {
+                    return "Scrolling up";
+                } else if (angle >= 135 || angle < -135) {
+                    return "Scrolling left";
+                } else if (angle >= -135 && angle < -45) {
+                    return "Scrolling down";
+                }
+            }
+        }
+
+        previousXCenter = centerX;
+        previousYCenter = centerY;
+        // 2. FEATURE - Zoom in/out
+        if (previousRectangleHeight != 0) {
+            double heightDifferenceFactor = 0.03;
+
+            // the height is normed [0.0, 1.0] to the camera window height.
+            // so the mouvement (when the hand is near the camera) should be equivalent to the mouvement when the hand is far.
+            double heightDifferenceThreshold = height * heightDifferenceFactor;
+            if (height < previousRectangleHeight - heightDifferenceThreshold) {
+                return "Zoom out";
+            } else if (height > previousRectangleHeight + heightDifferenceThreshold) {
+                return "Zoom in";
+            }
+        }
+        previousRectangleHeight = height;
+        // each odd Frame is skipped. For a better result.
+        frameCounter = !frameCounter;
+        if (frameCounter && multiHandLandmarks != null) {
+
+            for (NormalizedLandmarkList landmarks : multiHandLandmarks) {
+
+                List<LandmarkProto.NormalizedLandmark> landmarkList = landmarks.getLandmarkList();
+                NormalizedLandmark wrist = landmarkList.get(0);
+                NormalizedLandmark MCP_of_second_finger = landmarkList.get(9);
+
+                // angle between the hand (wirst and MCP) and the x-axis.
+                double ang_in_radian =
+                        getAngleABC(MCP_of_second_finger.getX(), MCP_of_second_finger.getY(),
+                                wrist.getX(), wrist.getY(), wrist.getX() + 0.1, wrist.getY());
+                int ang_in_degree = radianToDegree(ang_in_radian);
+                // LOG(INFO) << "Angle: " << ang_in_degree;
+                if (previousAngle != 0) {
+                    double angleDifferenceTreshold = 12;
+                    if (previousAngle >= 80 && previousAngle <= 100) {
+                        if (ang_in_degree > previousAngle + angleDifferenceTreshold) {
+                            return "Slide left";
+
+                        } else if (ang_in_degree < previousAngle - angleDifferenceTreshold) {
+                            return "Slide right";
+
+                        }
+                    }
+                }
+                previousAngle = ang_in_degree;
+            }
+
+        }
+        return "";
+    }
+
+
     boolean isThumbNearFirstFinger(LandmarkProto.NormalizedLandmark point1, LandmarkProto.NormalizedLandmark point2) {
         double distance = getEuclideanDistanceAB(point1.getX(), point1.getY(), point2.getX(), point2.getY());
         return distance < 0.1;
     }
 
-    double getEuclideanDistanceAB(float a_x, float a_y, float b_x, float b_y) {
+    double getEuclideanDistanceAB(double a_x, double a_y, double b_x, double b_y) {
         double dist = Math.pow(a_x - b_x, 2) + Math.pow(a_y - b_y, 2);
         return Math.sqrt(dist);
     }
+
+    double getAngleABC(double a_x, double a_y, double b_x, double b_y, double c_x, double c_y) {
+        double ab_x = b_x - a_x;
+        double ab_y = b_y - a_y;
+        double cb_x = b_x - c_x;
+        double cb_y = b_y - c_y;
+
+        double dot = (ab_x * cb_x + ab_y * cb_y);   // dot product
+        double cross = (ab_x * cb_y - ab_y * cb_x); // cross product
+
+        return Math.atan2(cross, dot);
+    }
+
+    int radianToDegree(double radian) {
+        return (int) Math.floor(radian * 180. / Math.PI + 0.5);
+    }
+
 }
