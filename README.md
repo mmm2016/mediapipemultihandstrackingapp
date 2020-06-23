8e2d0f96832ba262ce9c4cdbf4dea2d9ee79bfef
阿毛 <maogl@ktc.cn>
2020-06-23 09:00:41 +0800

    Update sample code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Gesture recognition through Mediapipe on Android
 
-see these [instructions](https://google.github.io/mediapipe/getting_started/android_archive_library.html) that use Android Archive (AAR) and Gradle，The source code is copied from MediaPipe's [multi-hand tracking gpu demo](https://github.com/jiuqiant/mediapipe_multi_hands_tracking_aar_example). The idea of gesture recognition comes from [Simple Hand Gesture Recognition Code - Hand tracking - Mediapipe](https://gist.github.com/TheJLifeX/74958cc59db477a91837244ff598ef4a)， [Hand movement recognition](https://github.com/TheJLifeX/mediapipe/tree/hand-mouvement-recognition)
+see these [instructions](https://google.github.io/mediapipe/getting_started/android_archive_library.html) that use Android Archive (AAR) and Gradle，The idea of gesture recognition comes from [Simple Hand Gesture Recognition Code - Hand tracking - Mediapipe](https://gist.github.com/TheJLifeX/74958cc59db477a91837244ff598ef4a)， [Hand movement recognition](https://github.com/TheJLifeX/mediapipe/tree/hand-mouvement-recognition)
 
 
 
--- app/src/main/java/com/example/mediapipemultihandstracking/MainActivity.java
+++ app/src/main/java/com/example/mediapipemultihandstracking/MainActivity.java
@@ -1,96 +1,54 @@
+// Copyright 2019 The MediaPipe Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.example.mediapipemultihandstracking;
 
-import android.graphics.SurfaceTexture;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.Size;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.mediapipe.components.CameraHelper;
-import com.google.mediapipe.components.CameraXPreviewHelper;
-import com.google.mediapipe.components.ExternalTextureConverter;
-import com.google.mediapipe.components.FrameProcessor;
-import com.google.mediapipe.components.PermissionHelper;
+import com.example.mediapipemultihandstracking.basic.BasicActivity;
 import com.google.mediapipe.formats.proto.LandmarkProto;
 import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmark;
 import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmarkList;
 import com.google.mediapipe.formats.proto.RectProto;
-import com.google.mediapipe.framework.AndroidAssetUtil;
 import com.google.mediapipe.framework.Packet;
 import com.google.mediapipe.framework.PacketCallback;
 import com.google.mediapipe.framework.PacketGetter;
-import com.google.mediapipe.glutil.EglManager;
 
 import java.util.List;
 
 /**
- * Main activity of MediaPipe example apps.
+ * Main activity of MediaPipe multi-hand tracking app.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BasicActivity {
     private static final String TAG = "MainActivity";
-    private static final String BINARY_GRAPH_NAME = "multi_hand_tracking_mobile_gpu.binarypb";
-    private static final String INPUT_VIDEO_STREAM_NAME = "input_video";
-    private static final String OUTPUT_VIDEO_STREAM_NAME = "output_video";
+
     private static final String OUTPUT_LANDMARKS_STREAM_NAME = "multi_hand_landmarks";
     private static final String OUTPUT_HAND_RECT = "multi_hand_rects";
-    private static final CameraHelper.CameraFacing CAMERA_FACING = CameraHelper.CameraFacing.FRONT;
-    // Flips the camera-preview frames vertically before sending them into FrameProcessor to be
-    // processed in a MediaPipe graph, and flips the processed frames back when they are displayed.
-    // This is needed because OpenGL represents images assuming the image origin is at the bottom-left
-    // corner, whereas MediaPipe in general assumes the image origin is at top-left.
-    private static final boolean FLIP_FRAMES_VERTICALLY = true;
+    private List<NormalizedLandmarkList> multiHandLandmarks;
 
     private TextView gesture;
     private TextView moveGesture;
 
-    static {
-        // Load all native libraries needed by the app.
-        System.loadLibrary("mediapipe_jni");
-        System.loadLibrary("opencv_java3");
-    }
-
-    // {@link SurfaceTexture} where the camera-preview frames can be accessed.
-    private SurfaceTexture previewFrameTexture;
-    // {@link SurfaceView} that displays the camera-preview frames processed by a MediaPipe graph.
-    private SurfaceView previewDisplayView;
-    // Creates and manages an {@link EGLContext}.
-    private EglManager eglManager;
-    // Sends camera-preview frames into a MediaPipe graph for processing, and displays the processed
-    // frames onto a {@link Surface}.
-    private FrameProcessor processor;
-    // Converts the GL_TEXTURE_EXTERNAL_OES texture from Android camera into a regular texture to be
-    // consumed by {@link FrameProcessor} and the underlying MediaPipe graph.
-    private ExternalTextureConverter converter;
-    // Handles camera access via the {@link CameraX} Jetpack support library.
-    private CameraXPreviewHelper cameraHelper;
-    List<NormalizedLandmarkList> multiHandLandmarks;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        previewDisplayView = new SurfaceView(this);
-        setupPreviewDisplayView();
         gesture = findViewById(R.id.gesture);
         moveGesture = findViewById(R.id.move_gesture);
-        // Initialize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
-        // binary graphs.
-        AndroidAssetUtil.initializeNativeAssetManager(this);
-        eglManager = new EglManager(null);
-        processor =
-                new FrameProcessor(
-                        this,
-                        eglManager.getNativeContext(),
-                        BINARY_GRAPH_NAME,
-                        INPUT_VIDEO_STREAM_NAME,
-                        OUTPUT_VIDEO_STREAM_NAME);
-        processor.getVideoSurfaceOutput().setFlipY(FLIP_FRAMES_VERTICALLY);
+
+
         processor.addPacketCallback(
                 OUTPUT_LANDMARKS_STREAM_NAME,
                 (packet) -> {
@@ -111,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
                                     + "] "
                                     + getMultiHandLandmarksDebugString(multiHandLandmarks));
                 });
-
         processor.addPacketCallback(
                 OUTPUT_HAND_RECT
                 , new PacketCallback() {
@@ -132,77 +89,6 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
-        PermissionHelper.checkAndRequestCameraPermissions(this);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        converter = new ExternalTextureConverter(eglManager.getContext());
-        converter.setFlipY(FLIP_FRAMES_VERTICALLY);
-        converter.setConsumer(processor);
-        if (PermissionHelper.cameraPermissionsGranted(this)) {
-            startCamera();
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        converter.close();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(
-            int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        PermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    private void setupPreviewDisplayView() {
-        previewDisplayView.setVisibility(View.GONE);
-        ViewGroup viewGroup = findViewById(R.id.preview_display_layout);
-        viewGroup.addView(previewDisplayView);
-        previewDisplayView
-                .getHolder()
-                .addCallback(
-                        new SurfaceHolder.Callback() {
-                            @Override
-                            public void surfaceCreated(SurfaceHolder holder) {
-                                processor.getVideoSurfaceOutput().setSurface(holder.getSurface());
-                            }
-
-                            @Override
-                            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-                                // (Re-)Compute the ideal size of the camera-preview display (the area that the
-                                // camera-preview frames get rendered onto, potentially with scaling and rotation)
-                                // based on the size of the SurfaceView that contains the display.
-                                Size viewSize = new Size(width, height);
-                                Size displaySize = cameraHelper.computeDisplaySizeFromViewSize(viewSize);
-                                // Connect the converter to the camera-preview frames as its input (via
-                                // previewFrameTexture), and configure the output width and height as the computed
-                                // display size.
-                                converter.setSurfaceTextureAndAttachToGLContext(
-                                        previewFrameTexture, displaySize.getWidth(), displaySize.getHeight());
-                            }
-
-                            @Override
-                            public void surfaceDestroyed(SurfaceHolder holder) {
-                                processor.getVideoSurfaceOutput().setSurface(null);
-                            }
-                        });
-    }
-
-    private void startCamera() {
-        cameraHelper = new CameraXPreviewHelper();
-        cameraHelper.setOnCameraStartedListener(
-                surfaceTexture -> {
-                    previewFrameTexture = surfaceTexture;
-                    // Make the display view visible to start showing the preview. This triggers the
-                    // SurfaceHolder.Callback added to (the holder of) previewDisplayView.
-                    previewDisplayView.setVisibility(View.VISIBLE);
-                });
-        cameraHelper.startCamera(this, CAMERA_FACING, /*surfaceTexture=*/ null);
     }
 
     private String getMultiHandLandmarksDebugString(List<NormalizedLandmarkList> multiHandLandmarks) {
@@ -232,7 +118,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return multiHandLandmarksStr;
     }
-    // finger states
 
     private String handGestureCalculator(List<NormalizedLandmarkList> multiHandLandmarks) {
         if (multiHandLandmarks.isEmpty()) {
@@ -246,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
 
         for (NormalizedLandmarkList landmarks : multiHandLandmarks) {
 
-            List<LandmarkProto.NormalizedLandmark> landmarkList = landmarks.getLandmarkList();
+            List<NormalizedLandmark> landmarkList = landmarks.getLandmarkList();
             float pseudoFixKeyPoint = landmarkList.get(2).getX();
             if (pseudoFixKeyPoint < landmarkList.get(9).getX()) {
                 if (landmarkList.get(3).getX() < pseudoFixKeyPoint && landmarkList.get(4).getX() < pseudoFixKeyPoint) {
@@ -371,7 +256,7 @@ public class MainActivity extends AppCompatActivity {
 
             for (NormalizedLandmarkList landmarks : multiHandLandmarks) {
 
-                List<LandmarkProto.NormalizedLandmark> landmarkList = landmarks.getLandmarkList();
+                List<NormalizedLandmark> landmarkList = landmarks.getLandmarkList();
                 NormalizedLandmark wrist = landmarkList.get(0);
                 NormalizedLandmark MCP_of_second_finger = landmarkList.get(9);
 
@@ -401,17 +286,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    boolean isThumbNearFirstFinger(LandmarkProto.NormalizedLandmark point1, LandmarkProto.NormalizedLandmark point2) {
+    private boolean isThumbNearFirstFinger(LandmarkProto.NormalizedLandmark point1, LandmarkProto.NormalizedLandmark point2) {
         double distance = getEuclideanDistanceAB(point1.getX(), point1.getY(), point2.getX(), point2.getY());
         return distance < 0.1;
     }
 
-    double getEuclideanDistanceAB(double a_x, double a_y, double b_x, double b_y) {
+    private double getEuclideanDistanceAB(double a_x, double a_y, double b_x, double b_y) {
         double dist = Math.pow(a_x - b_x, 2) + Math.pow(a_y - b_y, 2);
         return Math.sqrt(dist);
     }
 
-    double getAngleABC(double a_x, double a_y, double b_x, double b_y, double c_x, double c_y) {
+    private double getAngleABC(double a_x, double a_y, double b_x, double b_y, double c_x, double c_y) {
         double ab_x = b_x - a_x;
         double ab_y = b_y - a_y;
         double cb_x = b_x - c_x;
@@ -423,8 +308,7 @@ public class MainActivity extends AppCompatActivity {
         return Math.atan2(cross, dot);
     }
 
-    int radianToDegree(double radian) {
+    private int radianToDegree(double radian) {
         return (int) Math.floor(radian * 180. / Math.PI + 0.5);
     }
-
 }
